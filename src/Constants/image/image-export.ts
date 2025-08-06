16f6ddf9d652224b5bbaf322cd03cc39c32d546e
Ayush-Khedekar <ayushkhedekar07@gmail.com>
2025-08-06 19:04:53 +0530

    website

---
+++ src/Constants/image/image-export.ts
@@ -0,0 +1,21 @@
+import Ourmission from "./Ourmission.png";
+import photo1 from "./gallery/photo1.jpg";
+import photo2 from "./gallery/photo2.jpg";
+import photo3 from "./gallery/photo3.jpg";
+import photo4 from "./gallery/photo4.jpg";
+import photo5 from "./gallery/photo5.jpg";
+import photo6 from "./gallery/photo6.jpg";
+import photo7 from "./gallery/photo7.jpg";
+import photo8 from "./gallery/photo8.jpg";
+const images = {
+  Ourmission,
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5,
+  photo6,
+  photo7,
+  photo8,
+};
+export { images };
